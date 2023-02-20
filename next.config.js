738c939ba43bc4c6318fd20c05841451648b28a7
Tony <soli.ghanbary@gmail.com>
2023-02-20 08:27:16 -0800

    update next config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    NODE_ENV: process.env.NODE_ENV,
-    VERCEL_URL: process.env.VERCEL_URL,
-    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: "development",
+    VERCEL_URL: "trpc-app.vercel.app",
   },
 };
 
